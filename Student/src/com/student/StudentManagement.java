94879f5f9a77626fe2acda5539ee5eecb81b1cda
Shyam-GIT <120120116028@git.org.in>
2016-12-05 13:54:44 +0530

    rhj
    ujyr

--- Student/src/com/student/StudentManagement.java
+++ Student/src/com/student/StudentManagement.java
@@ -5,6 +5,6 @@ public class StudentManagement {
 	public static void main(String args[]){
 		System.out.println(" i am shyam");
 		System.out.println("Welcome ");
-		System.out.println("This will be added without conflict");
+		System.out.println("This will be added without conflict j");
 	}
 }
