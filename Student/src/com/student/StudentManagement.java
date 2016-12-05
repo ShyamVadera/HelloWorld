67660b70dce007704a872d5ea615afcd83370249
shyam.vadera <shyam.vaderaknowarth.com>
2016-12-05 12:57:23 +0530

    My 1st commit

--- Student/src/com/student/StudentManagement.java
+++ Student/src/com/student/StudentManagement.java
@@ -3,7 +3,7 @@ package com.student;
 public class StudentManagement {
 
 	public static void main(String args[]){
-		System.out.println(" World i am shyam");
+		System.out.println(" i am shyam");
 		System.out.println("Welcome again");
 	}
 }
