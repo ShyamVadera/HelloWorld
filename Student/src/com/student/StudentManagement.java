6350e07e122bc32852c1d1191ccf058951f7119d
ShyamVadera <vaderashyam93@gmail.com>
2016-12-05 13:05:31 +0530

    I have moddified some thing

--- Student/src/com/student/StudentManagement.java
+++ Student/src/com/student/StudentManagement.java
@@ -4,6 +4,7 @@ public class StudentManagement {
 
 	public static void main(String args[]){
 		System.out.println(" i am shyam");
-		System.out.println("Welcome again");
+		System.out.println("Welcome ");
+		System.out.println("This will be added without conflict");
 	}
 }
